fb8261b607dd829ec8fea055d5208c520601aad5
Juan De Jesus <jdejesus93@gmail.com>
2025-10-11 15:15:29 -0500

    feat(home): Fix NavLink props

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React from "react"
 
 interface Props {
     href: string,
-    children: React.ReactElement | React.ReactElement[]
+    children: React.ReactElement<string> | React.ReactElement<string>[]
 }
 
 const NavLink = ({ href, children }: Props) => (
